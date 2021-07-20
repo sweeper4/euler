2e9ca8c65eab8944215c6b0aa79f162cd41ff4ff
Sarati <friedchicken14821@gmail.com>
2021-07-20 02:31:36 -0500

    hi my name sarati

--- src/problems/problem1.rs
+++ src/problems/problem1.rs
@@ -1,15 +1,16 @@
-fn multiples_of_3_5(max : u32) -> u32 {
-    let correct = max - 1;
-    let sum_threes = 3 * sum_to(correct/3);
-    let sum_fives = 5 * sum_to(correct/5);
-    let sum_fifteens = 15 * sum_to(correct/15);
-    return sum_threes + sum_fives - sum_fifteens
-}
 
-fn sum_to(max: u32) -> u32 {
-    (max * (max + 1)) / 2
+fn multiples_of_3_5(upperLimit:u64) -> u64 {
+    let mut x: u64 = 0;
+    for i in 0..upperLimit {
+        if i%3 ==0{ 
+            x += i
+        } else if i%5 ==0 {
+            x += i
+        }
+    }
+    return x;
+    
 }
-
 pub fn problem1() {
     println!("sum of the threes and fives is {} lower than {}",multiples_of_3_5(1000),1000);
 }
