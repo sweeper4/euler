c4ce49651fffb5f74f807e9892161b86e9d591fe
Sarati <friedchicken14821@gmail.com>
2021-07-20 02:32:54 -0500

    wookie

--- src/problems/problem2.rs
+++ src/problems/problem2.rs
@@ -1,45 +1,20 @@
-fn sum_evens_less_than( max: usize) -> usize {
-    let mut fb_generator = Fb::new();
-    let mut i = 0;
-    let mut total = 0;
-    while fb_generator.get(i) < max {
-        if fb_generator.get(i) % 2 == 0 {
-            total = total + fb_generator.get(i)
-        }
-        i = i + 1
-    }
-    return total
-}
 
-struct Fb {
-    cache: Vec<usize>
-}
-
-impl Fb {
-    fn get( &mut self,index: usize) -> usize {
-        match index {
-            0 => return 1_usize,
-            1 => return 2_usize,
-            _ =>
-                match self.cache.get(index - 2) {
-                    Some( a ) => {
-                        return *a 
-                    },
-                    None => {
-                        let r = self.get(index - 2) + self.get( index - 1 );
-                        self.cache.push(r);
-                        return r
-                    }
-                }
+//:)
+fn some_function_called_here(upperLimit: u128) -> u128{
+    let mut x=1;
+    let mut y=0;
+    let mut count=0;
+    while x <= upperLimit {
+        x = x+y;
+        y = x-y;
+        if x%2 ==0{
+            count+=x;
         }
     }
-
-    fn new() -> Fb {
-        Fb {
-            cache: Vec::new()
-        }
-    }
+    return count;
+            
+    
 }
-pub fn problem2() {
-    println!("total of even fibonacci numbers = {}", sum_evens_less_than(4000000));
-}
+pub fn problem2(){
+    println!("bananas{}", some_function_called_here(400000000000000000000))
+} 
